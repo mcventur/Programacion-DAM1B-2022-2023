a6d8b41cc5e177c87d6a650cb74e800f4504bc4d
mcventur <marcelinopenide@gmail.com>
2022-10-07 16:43:22 +0200

    AD09 Personaje Videojuego

---
+++ UT3/AD09 Personaje Videojuego/Personaje.java
@@ -0,0 +1,98 @@
+
+/**
+ *  
+ * 
+ * @author  
+ * @version  
+ */
+public class Personaje
+{
+    //Constantes
+    private final char NORTE = 'N';
+    private final char SUR = 'S';
+    private final char ESTE = 'E';
+    private final char OESTE = 'O';
+    //Atributos 
+    private String nombre;
+    private int x;
+    private int y;
+    private char orientacion;
+
+    /**
+     * Constructor  
+     */
+    public Personaje(String queNombre)    {
+        nombre = queNombre;
+        x = 0;
+        y = 0;
+        orientacion = NORTE;
+    }
+
+    /**
+     * Modifica las coordenadas del personaje que
+     * avanzará la distancia (en unidades) especificada como parámetro
+     */
+    public void avanzar(int queDistancia) {
+        switch(orientacion){
+            case NORTE:
+                y += queDistancia;
+                //y = y + queDistancia;
+                break;
+            case SUR:
+                y -= queDistancia;
+                break;
+            case ESTE:
+                x += queDistancia;
+                break;
+            case OESTE:
+                x -= queDistancia;
+        }
+    }
+
+    /**
+     * cambia la orientación del personaje que siempre gira
+     * en sentido horario
+     */
+    public void girar() {
+        switch(orientacion){
+            case NORTE:
+                orientacion = ESTE;
+                break;
+            case ESTE:
+                orientacion = SUR;
+                break;
+            case SUR:
+                orientacion = OESTE;
+                break;
+            case OESTE:
+                orientacion = NORTE;
+        }
+    }
+    
+    public void girarV2() {
+       if(orientacion == ESTE){
+            orientacion = SUR;   
+        }            
+       else if(orientacion == NORTE){
+            orientacion = ESTE;
+        }
+            
+       else if(orientacion == OESTE){
+            orientacion = NORTE;
+        }       
+       else{
+            orientacion = OESTE;    
+        }
+    }    
+    
+    public String toString(){
+        return "Personaje = " + nombre + "\n" + 
+        "Orientacion " + orientacion + "\n" + 
+        "Posición [" + x + "," + y + "]\n"; 
+    }
+    
+}
+
+
+
+
